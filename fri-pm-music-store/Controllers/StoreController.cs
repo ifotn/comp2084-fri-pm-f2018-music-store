ff8f1582ae9efe023fe6cee58ee3f099ff2967dc
ifotn <rich@infrontofthenet.com>
2018-11-02 14:42:33 -0400

    lesson 8.  cart

--- fri-pm-music-store/Controllers/StoreController.cs
+++ fri-pm-music-store/Controllers/StoreController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using fri_pm_music_store.Models;
 
 namespace fri_pm_music_store.Controllers
 {
     public class StoreController : Controller
     {
+        // db connection - this is done automatically in scaffolded controllers
+        private MusicStoreModel db = new MusicStoreModel();
+
         // GET: Store
         public ActionResult Index()
         {
-            return View();
+            var genres = db.Genres.OrderBy(g => g.Name).ToList();
+            return View(genres);
+        }
+
+        // GET: Store/Albums?genre=Name
+        public ActionResult Albums(string genre)
+        {
+            // get albums for selected genre
+            var albums = db.Albums.Where(a => a.Genre.Name == genre).ToList();
+
+            // load view and pass the album list for display
+            return View(albums);
         }
 
         // GET: Store/Product
